cb02ed25501883e214f022cc5d696dd846a03c12
Jean-François Lambert <jf.lambert.it@gmail.com>
2022-08-05 07:31:10 -0400

    Client and site added

--- app/Models/site.php
+++ app/Models/site.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class site extends Model
+class Site extends Model
 {
     use HasFactory;
 }
