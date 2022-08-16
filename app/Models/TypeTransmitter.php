30143f13a2a1da0a89f445f86b5f9ba54c858199
Jean-François Lambert <jf.lambert.it@gmail.com>
2022-08-16 13:55:47 -0400

    added composante

---
+++ app/Models/TypeTransmitter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TypeTransmitter extends Model
+{
+    use HasFactory;
+}
